9e6876a8a13648209c306065e7fb2a43b9f50962
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-09-11 20:55:33 +0200

    commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
   const [resultText, setResultText] = useState('');
   const [number1, setnomber1] = React.useState('');
   const [number2, setnumber2] = React.useState('');
+  const [data, setData] = React.useState([]);
+
+  const addition = (event)=>{
+    setResultText(parseInt(number1) + parseInt(number2));
+    setData([...data,{ key: `${number1} + ${number2} = ${parseInt(number1) + parseInt(number2)}` }]);
+    
+  }
+  const soustraction = (event)=>{
+    setResultText(parseInt(number1) - parseInt(number2));
+    setData([...data,{ key: `${number1} - ${number2} = ${parseInt(number1) - parseInt(number2)}` }]);
+  }
+
   return (
     <View style={styles.container}>
       <View style={{flexDirection : 'row', alignItemes: 'center'}}>
@@ -14,9 +26,16 @@ export default function App() {
         <TextInput keyboardType='numeric' style={{width:200,borderColor:'gray',borderWidth:1, fontSize: 20}} value={number1} onChangeText={setnomber1} />
         <TextInput keyboardType='numeric' style={{width:200,borderColor:'gray',borderWidth:1, fontSize: 20}} value={number2} onChangeText={setnumber2} />
       <View style={{width: 100, height: 60, flexDirection : 'row', alignItemes: 'center', justifyContent: 'space-around', paddingVertical: 12}}>
-        <Button title=" + " onPress={() => setResultText(parseInt(number1) + parseInt(number2))} />
-        <Button title=" - " onPress={() => setResultText(parseInt(number1) - parseInt(number2))} />
+        <Button title=" + " onPress={addition} />
+        <Button title="  -  " onPress={soustraction} />
       </View>
+      <Text >History</Text>
+      <FlatList style={styles.list}
+        data={data}
+        renderItem={({ item }) =>
+        <Text>{item.key}</Text>
+      }
+    />
       <StatusBar style="auto" />
     </View>
   );
@@ -27,6 +46,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    marginVertical: 50,
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
   },
 });
